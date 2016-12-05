8aa00fe7e0bcb35193d6cc0c88fc69378ccabf53
JSai23 <jsaivarahi@gmail.com>
2016-12-05 15:56:33 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,6 +1,19 @@
-var path = require('path');
-
-app.get( '/', function( req, res ) {
+modules.export = function(app, pasport){
+    app.get( '/', function( req, res ) {
+    res.sendfile(__dirname + '/aboutus.html');
+  });
+    app.get( '/registration', function( req, res ) {
     res.sendfile(__dirname + '/registration.html');
   });
+    app.get( '/login', function( req, res ) {
+    res.sendfile(__dirname + '/sign_in.html');
+  });
+    app.get( '/welcome',isLoggedIn, function( req, res ) {
+    res.sendfile(__dirname + '/welcome.html');
+  });
+    app.get( '/logout', function( req, res ) {
+    req.logout();
+    req.redirect('/');
+  });
+};
 app.listen('3000');
