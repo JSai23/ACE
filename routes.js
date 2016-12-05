f569b237c70c945e2958b2f365af8e3510e4ea2e
JSai23 <jsaivarahi@gmail.com>
2016-12-05 15:37:52 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,8 +1,4 @@
-var express = require('express');
-var app = express();
 var path = require('path');
-
-app.use(express.static(__dirname));
 
 app.get( '/', function( req, res ) {
     res.sendfile(__dirname + '/registration.html');
