e310b4f996dcd547579e1637f95c5630596ac738
JSai23 <jsaivarahi@gmail.com>
2016-12-05 15:35:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+var express = require('express');
+var app = express();
+var flash = require('connect-flash');
+var passport = require('passport');
+var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+//files
+require('/routes.js')(app, passport);
+require('/passport.js')(passport);
+//now database
+mongoose.connect(......);
+//Parse cookies and body
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//Port
+console.log("Working Now");
+app.listen(3000);
