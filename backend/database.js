d619c70912435048d885670f0a93a0fe600ee6bf
JSai23 <jsaivarahi@gmail.com>
2017-02-05 13:00:43 -0500

    New Database

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://JSai:sssaaaiii@ds143539.mlab.com:43539/ace_db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://<dbuser>:<dbpassword>@ds143449.mlab.com:43449/ace_db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
